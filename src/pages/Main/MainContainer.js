782f956e3d6cd53c2278e1368c6f3f575f7f2354
mjheo <ganada7522@naver.com>
2023-03-27 15:35:04 +0900

    Fix: 메인 리뷰 리스트 출력 에러 고침

--- src/pages/Main/MainContainer.js
+++ src/pages/Main/MainContainer.js
@@ -11,42 +11,65 @@ function MainContainer() {
 
   let [id, setId] = useState('');
   let [email, setEmail] = useState('');
+  
+  
+  //리스트조회
   useEffect(() => {
-    axios.get(`http://localhost:8080/review?page=${page}`).then((res) => {
-      setList(res.data.content);
-    })
-  }, [page]);
+    async function fetchData() {
+      const response = await fetch(`http://localhost:8080/review?page=${page}`);
+      const data = await response.json();
+      setList(data);
+    }
+    fetchData();
+  }, []);
+  
+  // useEffect(() => {
+  //   axios.get(`http://localhost:8080/review?page=${page}`).then((res) => {
+  //     setList(res.data.content);
+  //   })
+  // }, []);
+
+
+
 
   useEffect(() => {
-    axios.get(`http://localhost:8080/oauth/user/info`, { withCredentials: true }).then((res) => {
-      console.log('res : ', res.data);
-      setId(res.data.nickname);
-      setEmail(res.data.email);
-    })
+    // 사용자 정보
+    // axios.get(`http://localhost:8080/oauth/user/info`, { withCredentials: true }).then((res) => {
+    //   console.log('res : ', res.data);
+    //   setId(res.data.nickname);
+    //   setEmail(res.data.email);
+    // })
   }, [])
 
 
   return (
     <div className='container text-center'>
-      로그인 정보 <br />
+      {/* 로그인 정보 <br />
       이메일 : {email} <br />
-      닉넴 : {id}
+      닉넴 : {id} */}
       <div className='text-end'>
         <TiPlus className='h1 pointer' onClick={() => navigate("/review")} />
       </div>
-      <div className='row row-cols-3'>
+      
         {
+          list.length > 0 ?
           list.map((review, i) => {
             return (
-              <div className='col' key={i}>
-                <Link to={`/review/${review.reviewId}`}>
-                  <img className="w-100 mt-5" src={`${process.env.PUBLIC_URL}/assets/${review.category.categoryName}.jpg`} alt={`image${i}`} />
-                </Link>
+              <div className='row row-cols-3'>
+                <div className='col' key={i}>
+                  <Link to={`/review/${review.reviewId}`}>
+                    <img className="w-100 mt-5" src={`${process.env.PUBLIC_URL}/assets/${review.category.categoryName}.jpg`} alt={`image${i}`} />
+                  </Link>
+                </div>
               </div>
             )
           })
+          :
+          <div className='text-center mt-5'>
+            <h3>Not exist review data</h3>
+          </div>
         }
-      </div>
+      
     </div>
   );
 }
